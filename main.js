2553168c46f410b323f46ab0e9b5282f404b5af6
kidohu <gnsia@gnsiaui-MacBookPro.local>
2022-03-20 21:01:25 +0900

    done

--- main.js
+++ main.js
@@ -6,50 +6,59 @@ var db = require('./lib/db');
 var topic = require('./lib/topic');
 var author = require('./lib/author');
 
-var app = http.createServer(function(request,response){
-    var _url = request.url;
-    var queryData = url.parse(_url, true).query;
-    var pathname = url.parse(_url, true).pathname;
-    if(pathname === '/'){
+var app = http.createServer(function (request, response) {
+  var _url = request.url;
+  var queryData = url.parse(_url, true).query;
+  var pathname = url.parse(_url, true).pathname;
+  if (pathname === '/') {
 
-      // main page
-      if(queryData.id === undefined){
-        topic.home(request, response);
+    // main page
+    if (queryData.id === undefined) {
+      topic.home(request, response);
 
       // other pages
-      } else {
-        topic.page(request, response);
-      }
-    
+    } else {
+      topic.page(request, response);
+    }
+
     //create
-    } else if(pathname === '/create'){
-      topic.create(request, response);
+  } else if (pathname === '/create') {
+    topic.create(request, response);
 
     // create_process
-    } else if(pathname === '/create_process'){
-      topic.create_process(request, response);
+  } else if (pathname === '/create_process') {
+    topic.create_process(request, response);
 
     // update
-    } else if(pathname === '/update'){
-      topic.update(request, response);
+  } else if (pathname === '/update') {
+    topic.update(request, response);
 
     //update_process  
-    } else if(pathname === '/update_process'){
-      topic.update_process(request, response);
+  } else if (pathname === '/update_process') {
+    topic.update_process(request, response);
 
     // delete_process
-    } else if(pathname === '/delete_process'){
-      topic.delete_process(request, response);
+  } else if (pathname === '/delete_process') {
+    topic.delete_process(request, response);
 
-    } else if(pathname === '/author'){
-      author.home(request, response);
+  } else if (pathname === '/author') {
+    author.home(request, response);
 
-    } else if(pathname === '/author/create_process'){
-      author.create_process(request, response);
+  } else if (pathname === '/author/create_process') {
+    author.create_process(request, response);
 
-    } else {
-      response.writeHead(404);
-      response.end('Not found');
-    }
+  } else if (pathname === '/author/update') {
+    author.update(request, response);
+
+  } else if (pathname === '/author/update_process') {
+    author.update_process(request, response);
+
+  } else if (pathname === '/author/delete_process') {
+    author.delete_process(request, response);
+
+  } else {
+    response.writeHead(404);
+    response.end('Not found');
+  }
 });
 app.listen(3000);
